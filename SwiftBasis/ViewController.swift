9a86d013e5eec37583d58ec2ca74a6ef583b42e3
Joelixy <hz504t@gmail.com>
2019-01-17 10:50:20 +0800

    init SwiftBasis

---
+++ SwiftBasis/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftBasis
+//
+//  Created by Joelixy on 2019/1/17.
+//  Copyright © 2019 JoeLixy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func grant()  {
+        print("Hello everyone!");
+    }
+}
+
